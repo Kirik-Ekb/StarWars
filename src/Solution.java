da4bf48922a124dc7eb87404b0699d732e2fced5
Vladimir Kirichenko <Kirik-Ekb@yandex.ru>
2021-07-16 18:13:41 +0500

    Start Project

---
+++ src/Solution.java
@@ -0,0 +1,12 @@
+public class Solution {
+    public static void main(String[] args){
+        print("The power is easy to use!");
+        print("The power opens many opportunities!");
+    }
+
+    public static void print(String a){
+        for (int i =0; i<=4;i++){
+            System.out.println(a);
+        }
+    }
+}
